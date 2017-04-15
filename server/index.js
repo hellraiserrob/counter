265fdeb67fc23d52e9837332ce08cd59e2a2260b
hellraiserrob <robertjamesphillips@googlemail.com>
2017-04-15 09:31:01 +0100

    heroku deployment process

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@
 const socket = require('socket.io')
 const express = require('express')
 const http = require('http')
-
 const app = express()
+const port = process.env.PORT || 5000;
+const path = require('path');
+
 const server = http.createServer(app)
 
 var io = socket.listen(server);
-io.set('origins = *');
+//io.set('origins = *');
 
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
@@ -29,6 +31,9 @@ const CounterCtrl = require('./controllers/counter.controller')
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+
 
 // connection to mongo lab 
 mongoose.connect(config.queryString, (err) => {
@@ -38,8 +43,8 @@ mongoose.connect(config.queryString, (err) => {
         return console.log(err)
     }
 
-    server.listen(3001, () => {
-        console.log('listening on 3001')
+    server.listen(port, () => {
+        console.log(`listening on ${port}`)
     })
 
 
@@ -55,10 +60,25 @@ mongoose.connect(config.queryString, (err) => {
             console.log(`disconnect...`)
         })
 
+        // routes
 
         app.post('/api/update', (req, res) => {
             CounterCtrl.update(req, res, socket)
         })
+
+        app.get('/api/getAll', (req, res) => {
+            CounterCtrl.getAll(req, res)
+        })
+
+        app.get('/getById', (req, res) => {
+            CounterCtrl.getById(req, res)
+        })
+
+        // All remaining requests return the React app, so it can handle routing.
+        app.get('*', (request, response) => {
+            response.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+        });
+
 
     })
 
@@ -66,18 +86,8 @@ mongoose.connect(config.queryString, (err) => {
 
 })
 
-// routes
-
-app.get('/api/getAll', (req, res) => {
-    CounterCtrl.getAll(req, res)
-})
-
-app.get('/getById', (req, res) => {
-    CounterCtrl.getById(req, res)
-})
 
 
 // app.get('*', (req, res) => {
 //     console.log('404')
 // })
-
